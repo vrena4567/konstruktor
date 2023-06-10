04fc7c2e512383d9f53a40f968d54c4fc88128e0
vrena4567 <v.rena4567@gmail.com>
2023-06-10 13:55:33 +0200

    added trials of ember

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
         People ember = new People();
         ember.setHappyScore(120);
+        System.out.println("happiness score of ember: " + ember.getHappyScore());
+        ember.setAge(18);
+        System.out.println("ember's age: " + ember.getAge());
+
 
     }
 }
